452c5726ffc2ac68193bc7a91d33d03088cac670
rick baker <baker.rickw@gmail.com>
2017-02-28 17:43:56 -0700

    initial save

---
+++ client/components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Nav extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <nav>
+                <div className="nav-wrapper">
+                <a href="#!" className="brand-logo">Logo</a>
+                <a href="#" data-activates="mobile" className="button-collapse"><i className="material-icons">menu</i></a>
+                <ul className="right hide-on-med-and-down">
+                    <li><a href="/">New Nav Home</a></li>
+                </ul>
+                <ul className="side-nav" id="mobile">
+                    <li><a href="/">New Nav</a></li>
+                </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Nav;
